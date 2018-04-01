5bd28eba21b0998444f8fa79d85c8a27df5e303f
James Otto <jfotto2@gmail.com>
2018-03-31 18:24:02 -0700

    hello Mr. Otto -- locolhost only

--- manifests/run-puppet.pp
+++ manifests/run-puppet.pp
@@ -1,14 +1,17 @@
 # Set up regular Puppet runs
-file { '/tmp/hello_otto.txt':
-  ensure  => file,
-  content => "Hello Mr. Otto \n",
-}
+node 'localhost' {
 
+  file { '/tmp/hello_otto.txt':
+    ensure  => file,
+    content => "Hello Mr. Otto -- localhost only\n",
+  }
+}
+  
 file { '/usr/local/bin/run-puppet':
   source => '/etc/puppetlabs/code/environments/production/files/run-puppet.sh',
   mode   => '0755',
 }
-
+  
 cron { 'run-puppet':
   command => '/usr/local/bin/run-puppet',
   hour    => '*',
